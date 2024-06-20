5109bb017c34da213b6a1415135d623d5e89dae9
Swaraj Adhikari <swaraj2065@gmail.com>
2024-06-19 18:19:46 -0700

    Initial commit of Neovim configuration

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+local M = {}
+M.ui = {
+  theme_toggle = {"tokyodark", "tokyonight"},
+  theme = "tokyonight",
+  transparency = true,
+}
+return M
---
+++ lua/plugins/configs/others.lua
@@ -0,0 +1,105 @@
+local M = {}
+local utils = require "core.utils"
+
+M.blankline = {
+  indentLine_enabled = 1,
+  filetype_exclude = {
+    "help",
+    "terminal",
+    "lazy",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "mason",
+    "nvdash",
+    "nvcheatsheet",
+    "",
+  },
+  buftype_exclude = { "terminal" },
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = false,
+  show_current_context = true,
+  show_current_context_start = true,
+}
+
+M.luasnip = function(opts)
+
+  require("luasnip").config.set_config(opts)
+
+  -- vscode format
+  require("luasnip.loaders.from_vscode").lazy_load()
+  require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+  -- snipmate format
+  require("luasnip.loaders.from_snipmate").load()
+  require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+  -- lua format
+  require("luasnip.loaders.from_lua").load()
+  require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+  -- custom snipptets
+  local ls = require "luasnip"
+  local s = ls.snippet
+  local i = ls.insert_node
+  local rep = require("luasnip.extras").rep
+  local fmt = require("luasnip.extras.fmt").fmt
+
+  ls.add_snippets("cpp", {
+    s(
+      "frpn", -- Snippet trigger
+      fmt( -- Format the snippet
+        [[
+#ifndef LOCAL 
+    freopen("{}.in", "r", stdin);
+    freopen("{}.out", "w", stdout);
+#endif
+            ]],
+        {
+          i(1), -- Placeholder for input file name
+          rep(1), -- Repeat the placeholder for output file name
+        }
+      )
+    ),
+  })
+  -- Define the "fastio" snippet
+  ls.add_snippets("cpp", {
+    s(
+      "fastio", -- Snippet trigger
+      fmt( -- Format the snippet
+        [[
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+            ]],
+        {} -- No placeholders or transformations needed
+      )
+    ),
+  })
+
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+      if
+        require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require("luasnip").session.jump_active
+      then
+        require("luasnip").unlink_current()
+      end
+    end,
+  })
+end
+
+M.gitsigns = {
+  signs = {
+    add = { text = "│" },
+    change = { text = "│" },
+    delete = { text = "󰍵" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "│" },
+  },
+  on_attach = function(bufnr)
+    utils.load_mappings("gitsigns", { buffer = bufnr })
+  end,
+}
+
+return M
