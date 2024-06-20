30b5977e52a8d54bbd3bb030ca79740fb6a7c04a
alphaGengar <150190618+alphaGengar@users.noreply.github.com>
2024-06-20 15:38:33 -0700

    Update lspconfig.lua

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -1,21 +1,42 @@
-dofile(vim.g.base46_cache .. "lsp")
-require "nvchad.lsp"
-
 local M = {}
-local utils = require "core.utils"
+local map = vim.keymap.set
+local conf = require("nvconfig").ui.lsp
 
--- export on_attach & capabilities for custom lspconfigs
+-- export on_attach & capabilities
 M.on_attach = function(client, bufnr)
-  utils.load_mappings("lspconfig", { buffer = bufnr })
+  local function opts(desc)
+    return { buffer = bufnr, desc = "LSP " .. desc }
+  end
 
-  if client.server_capabilities.signatureHelpProvider then
-    require("nvchad.signature").setup(client)
+  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+  map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
+  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
+  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+
+  map("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts "List workspace folders")
+
+  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+
+  map("n", "<leader>ra", function()
+    require "nvchad.lsp.renamer"()
+  end, opts "NvRenamer")
+
+  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
+  map("n", "gr", vim.lsp.buf.references, opts "Show references")
+
+  -- setup signature popup
+  if conf.signature and client.server_capabilities.signatureHelpProvider then
+    require("nvchad.lsp.signature").setup(client, bufnr)
   end
 end
 
--- disable semantic tokens
+-- disable semanticTokens
 M.on_init = function(client, _)
-  if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
+  if client.supports_method "textDocument/semanticTokens" then
     client.server_capabilities.semanticTokensProvider = nil
   end
 end
@@ -40,62 +61,33 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
-require("lspconfig").lua_ls.setup {
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+M.defaults = function()
+  dofile(vim.g.base46_cache .. "lsp")
+  require "nvchad.lsp"
 
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-          [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+  require("lspconfig").lua_ls.setup {
+    on_attach = M.on_attach,
+    capabilities = M.capabilities,
+    on_init = M.on_init,
+
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
         },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
-}
-
--- C++ LSP
-require("lspconfig").clangd.setup {
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-}
-
--- Python LSP (using pyright)
-require("lspconfig").pyright.setup {
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-}
-
--- Java LSP (using efm-langserver with jdtls)
-require("lspconfig").efm.setup {
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  init_options = { documentFormatting = true },
-  filetypes = { "java" },
-  settings = {
-    rootMarkers = { "pom.xml", ".git/" },
-    languages = {
-      java = {
-        {
-          formatCommand = "efm-langserver -c $HOME/.config/efm-langserver/config.yaml --stdin --stdout --filename ${INPUT}",
-          formatStdin = true,
+        workspace = {
+          library = {
+            vim.fn.expand "$VIMRUNTIME/lua",
+            vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
+            vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+            vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          },
+          maxPreload = 100000,
+          preloadFileSize = 10000,
         },
       },
     },
-  },
-}
+  }
+end
 
 return M
